d7f5b41bf653587b5147eb780382e640ea3b69fc
Kliment-Fis <51344164+Kliment-Fis@users.noreply.github.com>
2021-08-26 12:25:50 +0300

    Convert to OOP design

--- clock.py
+++ clock.py
@@ -1,73 +1,55 @@
 import pygame
 from datetime import datetime
 from time import sleep
-
+from random import randint
 
 pygame.init()
 
-# Setings/Настройки
-size = 30
-bg_color = (0, 0, 0)
-# color = (43, 255, 10)
-# color = (255, 0, 0) #Red 
-# color = (0, 255, 0) #Green
-# color = (0, 0, 255) #Blue
-# color = (255, 255, 255) #White
-# color = (0, 255, 255) #Aqua
-color = (255, 255, 0) #Yellow
-# color = (66, 170, 255) #WhiteBlue
+class Clock():
+	def __init__(self):
+		self.size = 30
+		self.bg_color = (0, 0, 0)
+		self.color = (255, 0, 0)
+		self.width = (3 * 5 + 6) * self.size 
+		self.height = 7 * self.size
+		# self.flags = pygame.NOFRAME | pygame.RESIZABLE
+		# self.display = pygame.display.set_mode((self.width, self.height), self.flags)
+		self.display = pygame.display.set_mode((self.width, self.height))
+		self.char = {
+			'0' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			'1' : [(2,0), (2,1), (2,2), (2,3), (2,4)],
+			'2' : [(0,0), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,4)],
+			'3' : [(0,0), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			'4' : [(0,0), (0,1), (0,2), (1,2), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			'5' : [(0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,2), (2,3), (2,4)],
+			'6' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,2), (2,3), (2,4)],
+			'7' : [(0,0), (1,0), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			'8' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			'9' : [(0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
+			':' : [(1,1), (1,3)]
+		}
 
-# Create display on setings template/ Создание дисплея на основе настроек
-width = (3 * 5 + 6) * size 
-height = 7 * size
-display = pygame.display.set_mode((width, height))
+	def Draw_number(self):
+		for i in range(len(self.time)):
+			for x in range(self.width):
+				for y in range(self.height):
+					if (x, y) in self.char[self.time[i:i+1]]:	
+						pygame.draw.rect(self.display, self.color, (x * self.size + (self.size * 1) + i * self.size * 4 + 1, y * self.size + (self.size * 1) + 1, self.size-2, self.size-2))
 
+	def Get_time(self):
+		self.time = datetime.now().strftime("%H:%M")
 
-char = {
-	'0' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	'1' : [(2,0), (2,1), (2,2), (2,3), (2,4)],
-	'2' : [(0,0), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,4)],
-	'3' : [(0,0), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	'4' : [(0,0), (0,1), (0,2), (1,2), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	'5' : [(0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,2), (2,3), (2,4)],
-	'6' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,2), (2,3), (2,4)],
-	'7' : [(0,0), (1,0), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	'8' : [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	'9' : [(0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,4), (2,0), (2,1), (2,2), (2,3), (2,4)],
-	':' : [(1,1), (1,3)],
-}
-
-def draw(x, y, i, color):
-	pygame.draw.rect(display, color, (x * size + (size * 1) + i * size * 4 + 1, y * size + (size * 1) + 1, size-2, size-2))
-
-
+	def Fill_display(self):
+		self.display.set_colorkey(self.bg_color)
 
 Game_over = False
 while not Game_over:
-	time = datetime.now().strftime("%H:%M")
 
-	# Fill background/ Заливка фона
-	for i in range(width):
-		for j in range(height):
-			draw(i - 1, j - 1, 0, bg_color)
-
-	# draw number/ Рисование чисел
-	for i in range(5):
-		for x in range(width):
-			for y in range(height):
-				if (x, y) in char[time[i:i+1]]:
-					draw(x, y, i, color)
-
-	# Update display/ Обновление дисплея
+	my_clock = Clock()
+	my_clock.Fill_display()
+	my_clock.Get_time()
+	my_clock.Draw_number()
 	pygame.display.update()
-
-	# Tick effect/ Эффект тиканья
-	draw(9, 1, 0, bg_color)
-	draw(9, 3, 0, bg_color)
-	sleep(0.5)
-
-	pygame.display.update()
-
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
